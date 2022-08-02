352e2f9783242a6f4073718a626e1ca5578db817
murakami <murakami@bitstar.jp>
2022-08-02 07:00:06 +0000

    ENTRYPOINTで、Djangoコマンドでデータを入れるのではなく、dumpからリストアすることで入れるように変更。

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -13,28 +13,10 @@ echo "mysql connect ok."
 
 pwd=`pwd`
 
+cd /tmp/dump/
+MYSQL_PWD=${DB_PASSWORD}
+mysql -h ${DB_HOST} -u ${DB_USER} ${DB_NAME} < ${DB_NAME}.dmp
+
 cd /usr/local/app/
-
-ls -lrth
-
-echo "----- makemigrations -----"
-python3 manage.py makemigrations
-echo "----- migrate -----"
-python3 manage.py migrate
-
-echo "----- truncate_all_data -----"
-python3 manage.py truncate_all_data
-echo "----- register_sex -----"
-python3 manage.py register_sex
-echo "----- register_district_prefecture -----"
-python3 manage.py register_district_prefecture
-
-echo "----- collectstatic -----"
 python3 manage.py collectstatic --noinput
-
-echo "----- runserver 0.0.0.0:8001 -----"
 python3 manage.py runserver 0.0.0.0:8001
-
-echo "----- register_person -----"
-python3 manage.py register_person
-
