055ebb0260c721bd92a2f69d44ee9cecfe626e08
murakami <murakami@bitstar.jp>
2022-08-04 17:20:37 +0000

    mariadbログ用ディレクトリを作成してパーミッションを変更する処理を追加。

--- start-docker-compose.sh
+++ start-docker-compose.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+# mariadb ログ用ディレクトリのパーミッションを777に
+VOL_LOG_PATH="./volumes/mysql/log/"
+
+if [ ! -d ${VOL_LOG_PATH} ]; then
+    sudo mkdir -p ${VOL_LOG_PATH}
+fi
+
+sudo chmod 777 ${VOL_LOG_PATH}
+
 docker compose up -d
